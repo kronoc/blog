7e2a480cec9e88cc0d7f90fe44036148cb0324fd
conor.keegan <con@conor.net>
2019-06-21 20:07:29 +0000

    making go.sh configurable

--- go.sh
+++ go.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+source ${HOME}/.config/blog
 set -eu
 rm -rf out
 ./make.py
-rsync -a --delete out/ /srv/conor.net/htdocs/radar
-rsync -a --delete files /srv/conor.net/htdocs/radar/
+rsync -a --delete out/ ${BLOG_HOST}:${BLOG_PATH}
+rsync -a --delete files ${BLOG_HOST}:${BLOG_PATH}/
+rsync -a --delete images ${BLOG_HOST}:${BLOG_PATH}/
